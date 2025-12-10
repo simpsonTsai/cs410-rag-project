432d5888c0e50c9d85da53a2ef3bd58d513d00c3
simpsonTsai <122127982+simpsonTsai@users.noreply.github.com>
2025-12-09 19:14:08 -0800

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+
+# ===============================
+# TA / Grading Configuration
+# ===============================
+
+TA_MODE = True   #Default True for grading
+
+# Limits for CPU-friendly execution
+TA_MAX_PAGES = 10
+TA_MAX_CHUNKS = 50
+TA_MAX_EMBED = 50
